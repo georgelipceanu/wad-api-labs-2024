ae756f641caa0bfc297be1403a505d4195010ae5
George <george.lipceanu@gmail.com>
2024-11-25 21:53:20 +0000

    Mongo lab Exercises done

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -7,4 +7,10 @@ const UserSchema = new Schema({
   password: {type: String, required: true }
 });
 
+const passwordValidator = (password) => {
+  const regexString = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  return regexString.test(password);
+}
+UserSchema.path("password").validate(passwordValidator);
+
 export default mongoose.model('User', UserSchema);
